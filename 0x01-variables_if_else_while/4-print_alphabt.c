198c83970e4cb692c999aa97bad3400448742c1f
NdipEstaban <estabannd@gmail.com>
2023-07-14 19:02:56 -0700

    added file /0x01/4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - entry point of program
+ * Return: 0
+*/
+int main(void)
+{
+        char ch;
+
+        ch = 'a';
+        while (ch <= 'z')
+        {
+		if (ch != 'q' || ch != 'e')
+		{
+			putchar(ch);
+                	ch++;
+		}
+        }
+        putchar('\n');
+        return (0);
+}
