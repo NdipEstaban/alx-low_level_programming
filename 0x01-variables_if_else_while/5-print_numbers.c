6d1b2e66d31a0cc11390347c1534d271fb2d301e
NdipEstaban <estabannd@gmail.com>
2023-07-14 19:20:18 -0700

    added file 0x01/5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - entry point of program
+ * Return:0
+*/
+int main(void)
+{
+	int num;
+	
+	num = 0;
+	while (num < 10)
+	{
+		printf("%i", num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
