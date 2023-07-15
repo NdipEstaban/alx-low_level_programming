7a5bafc0daae0e984691d10359ecae7d8ee18512
NdipEstaban <estabannd@gmail.com>
2023-07-14 18:44:53 -0700

    added file /0x01/2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - entry point of program
+ * Return: 0
+*/
+int main(void)
+{
+	char ch;
+	
+	ch = "a";
+	while (ch <= "z")
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar("\n");
+	return (0);
+}
