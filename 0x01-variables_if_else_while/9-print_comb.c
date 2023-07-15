84f2dc7c54307f1d25628d6710b41349807190ab
NdipEstaban <estabannd@gmail.com>
2023-07-14 20:01:34 -0700

    removed spaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,14 @@ int main(void)
 {
 	int i;
 
-       	for (i = 48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 		if (i != 57)
 		{
 			putchar(',');
 			putchar(' ');
-		}	
+		}
 	}
 	putchar('\n');
 	return (0);
