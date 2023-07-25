b4740cdb0dafed9c26ab4239f1ed093ee15d81de
NdipEstaban <estabannd@gmail.com>
2023-07-24 18:50:34 -0700

    added file 0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+/**
+ * reset_to_98 - takes a 'pointer to an int' parameter and
+ * 		updates it's value to 98
+ *
+ * @p: 'pointer to an int' parameter
+ *
+ * Return:Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98; 
+}
