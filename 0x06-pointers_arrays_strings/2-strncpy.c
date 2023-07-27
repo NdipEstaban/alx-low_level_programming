eaf0cc0e1d3587662f4127f762ba3fc36ebbf60b
NdipEstaban <estabannd@gmail.com>
2023-07-26 19:47:08 -0700

    corrected task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,36 @@
 /**
- * _strncpy - creates a copy of a string
+ * _strncpy - A function that copies a string.
  *
- * @dest: pointer to string to be copied
- * @src: pointer to location to be copied to
+ * @dest: pointer to destination input buffer
+ * @src: pointer to source input buffer
+ * @n: bytes of @src
  *
- * Return: pointer to the copied string
- */
+ * Return: @dest
+*/
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n && src[i] != '\0'){
-		dest[i] = str[i];
-		i++;
-	}
+	/**
+	 * iterate through src array
+	 * where if there is no null byte
+	 * among the first n bytes of source
+	 * the string placed in dest will not be
+	 * null terminated
+	*/
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	/**
+	 * if the length of source is less than n
+	 * write additional nullbytes to dest to
+	 * ensure that a total of n bytes is written
+	*/
 	while (i < n)
 	{
 		dest[i] = '\0';
 		i++;
 	}
+
 	return (dest);
 }
