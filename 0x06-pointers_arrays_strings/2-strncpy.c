0968b361eaf891256adc69afd989a0ac189492e3
NdipEstaban <estabannd@gmail.com>
2023-07-26 19:39:18 -0700

    done with task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+/**
+ * _strncpy - creates a copy of a string
+ *
+ * @dest: pointer to string to be copied
+ * @src: pointer to location to be copied to
+ *
+ * Return: pointer to the copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && dest[i] != '\0'){
+		desc[i] = str[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
