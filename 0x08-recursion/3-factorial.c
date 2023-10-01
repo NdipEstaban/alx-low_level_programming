71199943a937fc59c20b1467beb41a8c634b9cbd
NdipEstaban <estabannd@gmail.com>
2023-10-01 03:01:46 -0700

    task 3: factorial using recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,10 +7,7 @@
 */
 int factorial(int n)
 {
-	if (n < 0)		/*error -1 if number is negative*/
-		return (-1);
-	else if (n == 0)	/*return factorial of 0 = 1*/
+	if (n < 1)
 		return (1);
-	else			/*do factorial of n if its not less or equal to 0*/
-		return (n * factorial(n - 1));
+	return (n * factorial(n-1));
 }
