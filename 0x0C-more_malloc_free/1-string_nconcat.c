ce9962485142a12b9b641049713e54e96fddf35d
NdipEstaban <estabannd@gmail.com>
2023-10-13 13:10:41 -0700

    retried task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,45 @@
-#include<stdlib.h>
+#include <stdlib.h>
+#include "main.h"
+
 /**
- * string_nconocat - function that concatenates two strings
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
  *
- * @s1: 1st string
- * @s2: 2nd string
- * @n: characters of  @s2 to concatenate
- *
- * Return: new string made of @s1 and @n characters of @s2
+ * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int new_str_len;
-	int l1;
-	int l2;
-	char *new_str;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	l1 = 0;
-	l2 = 0;
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[l1] != '\0')
-		l1++;
-	while (s2[l2] != '\0')
-		l2++;
-	new_str = (char *) malloc((l1 + n + 1) * sizeof(char));
-	new_str_len = l1 + n;
-	for (l1 = 0; l1 <= new_str_len && s1[l1] != '\0'; l1++)
-		new_str[l1] = s1[l1];
-	for (l2 = 0 ; l1 <= new_str_len && s2[l2] != '\0'; l2++)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
+		return (NULL);
+
+	while (i < len1)
 	{
-		new_str[l1] = s2[l2];
-		l1++;
+		s[i] = s1[i];
+		i++;
 	}
-	new_str[l1] = '\0';
-	return (new_str);
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
